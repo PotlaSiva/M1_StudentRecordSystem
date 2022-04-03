8cd14aede3f45e9d16e2268d036e85f326020ab8
PotlaSiva <101462262+PotlaSiva@users.noreply.github.com>
2022-04-03 10:37:06 +0530

    Create test.c

---
+++ 3_Implimentation/test.c
@@ -0,0 +1,126 @@
+#include "prototypes.h"
+#include "unity.h"
+
+struct student std;
+void SetColor(int ForgC)
+{
+     WORD wColor;
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+          SetConsoleTextAttribute(hStdOut, wColor);
+     }
+     return;
+}
+
+void ClearConsoleToColors(int ForgC, int BackC)
+{
+     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     COORD coord = {0, 0};
+     DWORD count;
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+     SetConsoleTextAttribute(hStdOut, wColor);
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+          FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
+          SetConsoleCursorPosition(hStdOut, coord);
+     }
+     return;
+}
+
+void SetColorAndBackground(int ForgC, int BackC)
+{
+     WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);;
+     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+     return;
+}
+
+COORD coord = {0,0}; 
+void gotoxy(int x, int y){
+    coord.X = x; coord.Y = y; 
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void drawRectangle(){
+    int i, j;
+    gotoxy(0,0);
+    printf("%c",201);
+    for(i = 1; i < 78; i++){
+        gotoxy(i, 0);
+        printf("%c",205);
+    }
+    gotoxy(78,0);
+    printf("%c",187);
+    for(i = 1; i < 25; i++){
+        gotoxy(78, i);
+        if(i == 6){
+            printf("%c",185);
+        }else{
+            printf("%c",186);
+        }
+    }
+    gotoxy(78, 25);
+    printf("%c",188);
+    for(i = 77; i > 0; i--){
+        gotoxy(i,25);
+        if(i == 35){
+            printf("%c",202);
+        }else{
+            printf("%c",205);
+        }
+    }
+    gotoxy(0,25);
+    printf("%c",200);
+    for(i = 24; i > 0; i--){
+        gotoxy(0,i);
+        if(i == 6){
+            printf("%c",204);
+        }else{
+            printf("%c",186);
+        }
+    }
+
+    for(i = 1; i < 78; i++){
+        gotoxy(i,6);
+        if(i == 35){
+            printf("%c",203);
+        }else{
+            printf("%c",205);
+        }
+    }
+
+    for(i = 7; i < 25; i++){
+        gotoxy(35,i);
+        printf("%c",186);
+    }
+
+}
+
+void clearWindow(){
+    int i,j;
+    for(i = 37; i < 78; i++){
+        for(j = 7; j < 25; j++){
+            gotoxy(i,j);printf(" ");
+        }
+    }
+    return;
+}
+
+void window(){
+    drawRectangle();
+    gotoxy(28,2);
+    SetColor(35);
+    printf("STUDENT PLACEMENT ");
+    gotoxy(16,3);
+    printf("MALINENI LAKSHMI ENGINEERING COLLEGE, Guntur, AP");
+    gotoxy(31,4);
+    printf("PIN: 522017");
+    gotoxy(25,24);
+    SetColor(17);
+
+}
